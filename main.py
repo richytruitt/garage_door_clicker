3aa6827a9f45399ca0cb8d83ac05b8bf5c6e8ad3
richytruitt <truittrichy@gmail.com>
2021-10-29 19:10:47 -0400

    Adding form

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 from flask import Flask
+# import RPi.GPIO as GPIO    # Import Raspberry Pi GPIO library
+from time import sleep
+from flask import Flask, request, redirect, render_template, url_for
+from jinja2 import Environment, FileSystemLoader
+
 
 app = Flask(__name__)
+# GPIO.setwarnings(False)    # Ignore warning for now
+# GPIO.setmode(GPIO.BOARD)   # Use physical pin numbering
+# GPIO.setup(8, GPIO.OUT, initial=GPIO.LOW)
 
 @app.route('/')
 def hello_world():
-    return 'Hey, we have Flask in a Docker container!'
+    return render_template('main.html')
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0', port='5555')
